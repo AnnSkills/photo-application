7ef6c720a181f768cd3a228de79c2569341c01d7
Anna <khomiakova25@gmail.com>
2022-10-11 11:54:46 +0300

    assign stripe customer_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,19 @@ class User < ApplicationRecord
          :confirmable,
          :trackable
 
+  def to_s
+    email
+  end
+
   private
 
   def after_confirmation
     WelcomeMailer.send_greeting(self).deliver_now
+  end
+
+  after_create do
+    customer = Stripe::Customer.create(email: self.email)
+    update(stripe_customer_id: customer.id)
   end
 
 end
